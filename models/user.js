cbacb6cd0a2e514dc3acf82da30010691a929b72
Austin Smith <austinsmith7531@gmail.com>
2019-06-21 00:40:12 -0400

    asdfs

--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    "Users",
-    {
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
-    },
-    {}
-  );
-  // User.associate = function (models) {
-  //   // associations can be defined here
-  // };
-  return User;
-};
+// module.exports = (sequelize, DataTypes) => {
+//   const User = sequelize.define(
+//     "Users",
+//     {
+//       firstName: DataTypes.STRING,
+//       lastName: DataTypes.STRING,
+//       email: DataTypes.STRING,
+//       password: DataTypes.STRING
+//     },
+//     {}
+//   );
+//   // User.associate = function (models) {
+//   //   // associations can be defined here
+//   // };
+//   return User;
+// };
